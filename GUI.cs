8c57c193b206014982650dd8c559266f021c22b7
Tricky1975 <facebook@tbbs.nl>
2018-11-14 00:09:27 +0100

    Auto down scroll console

--- GUI.cs
+++ GUI.cs
@@ -35,140 +35,155 @@ namespace Devlog
 {
     delegate void GenCallBack(object sender, EventArgs a);
 
-    public static class GUI
-    {
-        static readonly List<Widget> RequireProject = new List<Widget>();
-        static readonly List<Widget> RequireTag = new List<Widget>();
-        static MainWindow win;
-        static Notebook Tabber;
-        static TextView Console;
-        static Entry Prompt;
-        static ListBox ProjectList;
-        static ListBox TagList;
-        static HBox TagEditBox;
-        static Entry TagEditHead;
-        static Entry TagEditEntry;
-        static bool AllowEdit = true;
-        static public string CurrentProjectName = "";
-        static dvProject CurrentProject { get { if (CurrentProjectName=="") return null; return dvProject.Get(CurrentProjectName); } }// Acutal return comes later!
-        static Gdk.Color EntryLabel = new Gdk.Color(0, 180, 255);
+	public static class GUI
+	{
+		static readonly List<Widget> RequireProject = new List<Widget>();
+		static readonly List<Widget> RequireTag = new List<Widget>();
+		static MainWindow win;
+		static Notebook Tabber;
+		static TextView Console;
+		static Entry Prompt;
+		static ListBox ProjectList;
+		static ListBox TagList;
+		static HBox TagEditBox;
+		static Entry TagEditHead;
+		static Entry TagEditEntry;
+		static bool AllowEdit = true;
+		static public string CurrentProjectName = "";
+		static dvProject CurrentProject { get { if (CurrentProjectName == "") return null; return dvProject.Get(CurrentProjectName); } }// Acutal return comes later!
+		static Gdk.Color EntryLabel = new Gdk.Color(0, 180, 255);
 
 
-        static Dictionary<string, Entry> GenEntries = new Dictionary<string, Entry>();
+		static Dictionary<string, Entry> GenEntries = new Dictionary<string, Entry>();
 
-        static void AndACTION(object sender,EventArgs a){
-            CommandClass.DoCommand(Prompt.Text);
-            Prompt.Text = "";
-        }
+		static void AndACTION(object sender, EventArgs a)
+		{
+			CommandClass.DoCommand(Prompt.Text);
+			Prompt.Text = "";
+		}
 
-        static Entry GeneralAdd(VBox Panel, string codename, string Caption){ //,GenCallBack MyCallBack){
-            var e = new Entry();
-            var l = new Label(Caption);
-            l.ModifyFg(StateType.Normal, EntryLabel);
-            var b = new HBox();
-            b.SetSizeRequest(1000, 30);
-            e.SetSizeRequest(500, 30);
-            l.SetSizeRequest(500, 30);
-            b.Add(l);
-            b.Add(e);
-            Panel.Add(b);
-            RequireProject.Add(e);
-            GenEntries[codename] = e;
-            // e.Changed += MyCallBack; // C# FAILED!
-            return e;
-        }
+		static Entry GeneralAdd(VBox Panel, string codename, string Caption)
+		{ //,GenCallBack MyCallBack){
+			var e = new Entry();
+			var l = new Label(Caption);
+			l.ModifyFg(StateType.Normal, EntryLabel);
+			var b = new HBox();
+			b.SetSizeRequest(1000, 30);
+			e.SetSizeRequest(500, 30);
+			l.SetSizeRequest(500, 30);
+			b.Add(l);
+			b.Add(e);
+			Panel.Add(b);
+			RequireProject.Add(e);
+			GenEntries[codename] = e;
+			// e.Changed += MyCallBack; // C# FAILED!
+			return e;
+		}
 
-        static void GeneralInit(VBox Panel){
-            Panel.ModifyBg(StateType.Normal, new Gdk.Color(0, 0, 20));
-            GeneralAdd(Panel, "GITHUB", "Github Repository:").Changed += delegate (object s, EventArgs e) { if (AllowEdit) { CurrentProject.GitHub = ((Entry)s).Text; } };
-            GeneralAdd(Panel, "TARGET", "Site Target Dir:").Changed += delegate (object s, EventArgs e) { if (AllowEdit) { CurrentProject.Target = ((Entry)s).Text; } };
-            GeneralAdd(Panel, "TEMPLATE", "Template file:").Changed += delegate (object s, EventArgs e) { if (AllowEdit) { CurrentProject.Template = ((Entry)s).Text; } };
-        }
+		static void GeneralInit(VBox Panel)
+		{
+			Panel.ModifyBg(StateType.Normal, new Gdk.Color(0, 0, 20));
+			GeneralAdd(Panel, "GITHUB", "Github Repository:").Changed += delegate (object s, EventArgs e) { if (AllowEdit) { CurrentProject.GitHub = ((Entry)s).Text; } };
+			GeneralAdd(Panel, "TARGET", "Site Target Dir:").Changed += delegate (object s, EventArgs e) { if (AllowEdit) { CurrentProject.Target = ((Entry)s).Text; } };
+			GeneralAdd(Panel, "TEMPLATE", "Template file:").Changed += delegate (object s, EventArgs e) { if (AllowEdit) { CurrentProject.Template = ((Entry)s).Text; } };
+		}
 
-        static void AutoEnable(){
-            var b = CurrentProject != null;
-            foreach (Widget w in RequireProject) w.Sensitive = b;
+		static void AutoEnable()
+		{
+			var b = CurrentProject != null;
+			foreach (Widget w in RequireProject) w.Sensitive = b;
 			foreach (Widget w in RequireTag) w.Sensitive = b && TagList.ItemText != "";
-        }
+		}
 
-        static public void RenewProjects(){
-            ProjectList.Clear();
-            string[] filelist;
-            string workspace = MainClass.GetConfig("WORKSPACE");
-            if (workspace=="") {
-                QuickGTK.Error("I don't know where to look for projects! Please use the command GLOBALCONFIG WORKSPACE=/home/username/MyWorkSpace/ or something like that");
-                return;
-            }
-            try{
-                filelist = FileList.GetDir(workspace+"/Projects",0,true,false);
-            } catch {
-                QuickGTK.Error("Reading the project folder failed! Has it been properly configured?");
-                return;
-            }
-            foreach(string f in filelist){
-                if (qstr.Suffixed(f, ".prj")) ProjectList.AddItem(qstr.Left(f,f.Length-4));
-            }
-        }
+		static public void RenewProjects()
+		{
+			ProjectList.Clear();
+			string[] filelist;
+			string workspace = MainClass.GetConfig("WORKSPACE");
+			if (workspace == "")
+			{
+				QuickGTK.Error("I don't know where to look for projects! Please use the command GLOBALCONFIG WORKSPACE=/home/username/MyWorkSpace/ or something like that");
+				return;
+			}
+			try
+			{
+				filelist = FileList.GetDir(workspace + "/Projects", 0, true, false);
+			}
+			catch
+			{
+				QuickGTK.Error("Reading the project folder failed! Has it been properly configured?");
+				return;
+			}
+			foreach (string f in filelist)
+			{
+				if (qstr.Suffixed(f, ".prj")) ProjectList.AddItem(qstr.Left(f, f.Length - 4));
+			}
+		}
 
 
 
-        static void InitSidebar(VBox sidebar){
-            var lb = new ListBox("Projects"); ProjectList = lb;
-            var sw = new ScrolledWindow();
-            sw.Add(lb.Gadget);
-            Assembly asm = Assembly.GetExecutingAssembly();
-            System.IO.Stream stream;
-            //= asm.GetManifestResourceStream("MyData.Properties.Icon.png");
-            //Gdk.Pixbuf PIX = new Gdk.Pixbuf(stream);
-            //win.Icon = PIX;
-            //stream.Dispose();
-            stream = asm.GetManifestResourceStream("Devlog.Mascot.Mascot.png");
-            var mascot = new Image(stream);
-            mascot.SetAlignment((float)0.5, 2);
-            stream.Dispose();
-            sidebar.Add(sw);
-            sidebar.Add(mascot);
-            lb.Gadget.CursorChanged += delegate (object sender, EventArgs a) { Use(lb.ItemText); };
-            lb.Gadget.RulesHint = true;
-        }
+		static void InitSidebar(VBox sidebar)
+		{
+			var lb = new ListBox("Projects"); ProjectList = lb;
+			var sw = new ScrolledWindow();
+			sw.Add(lb.Gadget);
+			Assembly asm = Assembly.GetExecutingAssembly();
+			System.IO.Stream stream;
+			//= asm.GetManifestResourceStream("MyData.Properties.Icon.png");
+			//Gdk.Pixbuf PIX = new Gdk.Pixbuf(stream);
+			//win.Icon = PIX;
+			//stream.Dispose();
+			stream = asm.GetManifestResourceStream("Devlog.Mascot.Mascot.png");
+			var mascot = new Image(stream);
+			mascot.SetAlignment((float)0.5, 2);
+			stream.Dispose();
+			sidebar.Add(sw);
+			sidebar.Add(mascot);
+			lb.Gadget.CursorChanged += delegate (object sender, EventArgs a) { Use(lb.ItemText); };
+			lb.Gadget.RulesHint = true;
+		}
 
-        static public void Use(string prj){
-            System.Console.WriteLine($"User picked {prj}");
-            CurrentProjectName = prj;
-            UpdateTags();
-            AutoEnable();
-        }
+		static public void Use(string prj)
+		{
+			System.Console.WriteLine($"User picked {prj}");
+			CurrentProjectName = prj;
+			UpdateTags();
+			AutoEnable();
+		}
 
-        static void TagsInit(VBox panel){
-            TagList = new ListBox("Tags");
-            TagEditBox = new HBox();
+		static void TagsInit(VBox panel)
+		{
+			TagList = new ListBox("Tags");
+			TagEditBox = new HBox();
 			TagEditBox.SetSizeRequest(1000, 30);
-            var sw = new ScrolledWindow(); panel.Add(sw);
-            var tv = TagList.Gadget; sw.Add(tv);
-            var lb1 = new Label("Head"); lb1.ModifyFg(StateType.Normal, new Gdk.Color(0, 180, 255));
-            var lb2 = new Label("Content"); lb2.ModifyFg(StateType.Normal, new Gdk.Color(0, 180, 255));
-            TagEditHead = new Entry(); RequireTag.Add(TagEditHead);
-            TagEditEntry = new Entry(); RequireTag.Add(TagEditEntry);
-            TagEditBox.Add(lb1);
-            TagEditBox.Add(TagEditHead);
-            TagEditBox.Add(lb2);
-            TagEditBox.Add(TagEditEntry);
-            panel.Add(TagEditBox);
-            TagList.Gadget.RulesHint = true;
-            RequireProject.Add(TagList.Gadget);
+			var sw = new ScrolledWindow(); panel.Add(sw);
+			var tv = TagList.Gadget; sw.Add(tv);
+			var lb1 = new Label("Head"); lb1.ModifyFg(StateType.Normal, new Gdk.Color(0, 180, 255));
+			var lb2 = new Label("Content"); lb2.ModifyFg(StateType.Normal, new Gdk.Color(0, 180, 255));
+			TagEditHead = new Entry(); RequireTag.Add(TagEditHead);
+			TagEditEntry = new Entry(); RequireTag.Add(TagEditEntry);
+			TagEditBox.Add(lb1);
+			TagEditBox.Add(TagEditHead);
+			TagEditBox.Add(lb2);
+			TagEditBox.Add(TagEditEntry);
+			panel.Add(TagEditBox);
+			TagList.Gadget.RulesHint = true;
+			RequireProject.Add(TagList.Gadget);
 			TagList.Gadget.CursorChanged += OnTagSelect;
 			TagEditHead.Changed += OnEditTag;
 			TagEditEntry.Changed += OnEditTag;
-        }
+		}
 
-        static public void UpdateTags(){
-            TagList.Clear();
-            if (CurrentProject == null) return;
+		static public void UpdateTags()
+		{
+			TagList.Clear();
+			if (CurrentProject == null) return;
 			CurrentProject.Data.List("Tags").Sort();
-			foreach (string tag in CurrentProject.Data.List("Tags")) if (tag.Trim()!="") TagList.AddItem(tag.Trim());
-        }
+			foreach (string tag in CurrentProject.Data.List("Tags")) if (tag.Trim() != "") TagList.AddItem(tag.Trim());
+		}
 
-		static void OnTagSelect(object sender,EventArgs a){
+		static void OnTagSelect(object sender, EventArgs a)
+		{
 			AllowEdit = false;
 			var tag = TagList.ItemText;
 			TagEditHead.Text = CurrentProject.GetData($"HEAD.{tag}");
@@ -177,15 +192,18 @@ namespace Devlog
 			AutoEnable();
 		}
 
-		static void OnEditTag(object sender,EventArgs a){
+		static void OnEditTag(object sender, EventArgs a)
+		{
 			if (!AllowEdit) return;
 			var s = (Entry)sender;
 			var h = "INHD";
-			if (s==TagEditHead) h = "HEAD";
-			CurrentProject.DefData($"{h}.{TagList.ItemText}",s.Text);
+			if (s == TagEditHead) h = "HEAD";
+			CurrentProject.DefData($"{h}.{TagList.ItemText}", s.Text);
 		}
 
-        public static void Init()
+		static void ConsoleDOWN(object sender, EventArgs e) { Console.ScrollToIter(Console.Buffer.EndIter, 0, false, 0, 0); }
+
+	    public static void Init()
         {
             MKL.Lic    ("Development Log - GUI.cs","GNU General Public License 3");
             MKL.Version("Development Log - GUI.cs","18.11.14");
@@ -200,6 +218,7 @@ namespace Devlog
             Console = new TextView();
             Console.Editable = false;
 			Console.ModifyFont(Pango.FontDescription.FromString("Courier 18"));
+			Console.SizeAllocated += new SizeAllocatedHandler(ConsoleDOWN);
 			Prompt = new Entry();
             var overlord = new VBox();
             var superior = new HBox();
