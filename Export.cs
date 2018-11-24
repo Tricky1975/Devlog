006f5952a00f5459238b6ecac29dc605f192bb29
Tricky1975 <facebook@tbbs.nl>
2018-11-24 09:29:41 +0100

    Fixed: Generate issues: - Errors during generation when records were deleted - Putting in false records in the html output

--- Export.cs
+++ Export.cs
@@ -73,35 +73,37 @@ namespace Devlog
 			pageline = pageline.Trim();
 			var content = $"<table style=\"width:{cp.GetDataDefaultInt("EXPORT.TABLEWIDTH", 1200)}\">\n";
 			content += $"<tr><td colspan=3 align=center>{pageline}</td></tr>\n";
-			for (int i = cp.HighestRecordNumber; i > 0;i--){
+			for (int i = cp.HighestRecordNumber; i > 0; i--){
 				//if (i % 6 == 0) { GUI.Write("."); Console.Write($".{i}."); }
 				justnewpaged = false;
-				var rec = new dvEntry(cp, i);
-				if (rec.Date != olddate) content += $"<tr><td align=center colspan=3 style='font-size:30pt;'>- = {rec.Date} = -</td></tr>\n"; olddate = rec.Date;
-				string headstyle = cp.Data.C($"HEAD.{rec.Tag.ToUpper()}");
-				string contentstyle = cp.Data.C($"INHD.{rec.Tag.ToUpper()}");
-				content += $"<tr valign=top><td align=left><a id='dvRec_{rec.RecID}'></a>{rec.Time}</td><td style=\"{headstyle}\">{rec.Tag}</td><td style='width: { cp.GetDataDefaultInt("EXPORT.CONTENTWIDTH", 800)}; {contentstyle}'><div style=\"width: { cp.GetDataDefaultInt("EXPORT.CONTENTWIDTH", 800)}; overflow-x:auto;\">";
-				var icon = $"{OutDir}/Icons/{rec.Tag.ToLower()}";
-				icon = icon.Replace("#", "hashtag");
-				foreach(string pfmt in iconext) {
-					var iconfile = $"{icon}.{pfmt}";
-					iconfile = iconfile.Replace("#", "%23");
-					if (System.IO.File.Exists(iconfile)) { content += $"<img style='float:{cp.GetDataDefault("EXPORT.ICONFLOATPOSITION", "Right")}; height:{cp.GetDataDefaultInt("EXPORT.ICONHEIGHT", 50)}' src='{iconfile}' alt='{rec.Tag}'>"; break; }
-				}
-				content += $"{rec.Text}</div></td></tr>\n";
-				pcountdown--;
-				if (pcountdown<=0){
-					pcountdown = 200;
-					justnewpaged = true;
-					content += $"<tr><td colspan=3 align=center>{pageline}</td></tr>\n</table>\n\n";
-					QOpen.SaveString($"{OutDir}/{cp.PrjName}_DevLog_Page_{page}.html",template.Replace("@CONTENT@",content));
-					page++;
-					pageline = "";
-					for (int p = 1; p <= pages; p++) {
-						if (page == p) pageline += $"<big><big>{p}</big></big> "; else pageline += $"<a href='{cp.PrjName}_DevLog_Page_{p}.html'>{p}</a> ";
+				var rec = new dvEntry(cp, i,true);
+				if (rec.Loaded) {
+					if (rec.Date != olddate) content += $"<tr><td align=center colspan=3 style='font-size:30pt;'>- = {rec.Date} = -</td></tr>\n"; olddate = rec.Date;
+					string headstyle = cp.Data.C($"HEAD.{rec.Tag.ToUpper()}");
+					string contentstyle = cp.Data.C($"INHD.{rec.Tag.ToUpper()}");
+					content += $"<tr valign=top><td align=left><a id='dvRec_{rec.RecID}'></a>{rec.Time}</td><td style=\"{headstyle}\">{rec.Tag}</td><td style='width: { cp.GetDataDefaultInt("EXPORT.CONTENTWIDTH", 800)}; {contentstyle}'><div style=\"width: { cp.GetDataDefaultInt("EXPORT.CONTENTWIDTH", 800)}; overflow-x:auto;\">";
+					var icon = $"{OutDir}/Icons/{rec.Tag.ToLower()}";
+					icon = icon.Replace("#", "hashtag");
+					foreach (string pfmt in iconext) {
+						var iconfile = $"{icon}.{pfmt}";
+						iconfile = iconfile.Replace("#", "%23");
+						if (System.IO.File.Exists(iconfile)) { content += $"<img style='float:{cp.GetDataDefault("EXPORT.ICONFLOATPOSITION", "Right")}; height:{cp.GetDataDefaultInt("EXPORT.ICONHEIGHT", 50)}' src='{iconfile}' alt='{rec.Tag}'>"; break; }
 					}
-					content = $"<table style=\"width:{cp.GetDataDefaultInt("EXPORT.TABLEWIDTH", 1200)}\">\n";
-					content += $"<tr><td colspan=3 align=center>{pageline}</td></tr>\n";
+					content += $"{rec.Text}</div></td></tr>\n";
+					pcountdown--;
+					if (pcountdown <= 0) {
+						pcountdown = 200;
+						justnewpaged = true;
+						content += $"<tr><td colspan=3 align=center>{pageline}</td></tr>\n</table>\n\n";
+						QOpen.SaveString($"{OutDir}/{cp.PrjName}_DevLog_Page_{page}.html", template.Replace("@CONTENT@", content));
+						page++;
+						pageline = "";
+						for (int p = 1; p <= pages; p++) {
+							if (page == p) pageline += $"<big><big>{p}</big></big> "; else pageline += $"<a href='{cp.PrjName}_DevLog_Page_{p}.html'>{p}</a> ";
+						}
+						content = $"<table style=\"width:{cp.GetDataDefaultInt("EXPORT.TABLEWIDTH", 1200)}\">\n";
+						content += $"<tr><td colspan=3 align=center>{pageline}</td></tr>\n";
+					}
 				}
 			}
 			if (!justnewpaged) {
